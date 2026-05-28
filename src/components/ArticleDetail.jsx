dccf08e34b54977edfb700761cf650f21647eb64
Salasevicius <salasevicius1@gmail.com>
2026-05-28 14:17:08 -0300

    Correcciones en ArticleDetail

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -47,6 +47,11 @@ const ArticleDetail = () => {
         <title>{`${noticia.title} | Proyecto Cultura Rosario`}</title>
         <meta name="description" content={noticia.description} />
 
+        {/* CONEXIÓN CON GOOGLE FONTS PARA LIBRE CASLON TEXT */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+
         {/* Open Graph / Facebook / WhatsApp */}
         <meta property="og:type" content="article" />
         <meta property="og:title" content={noticia.title} />
